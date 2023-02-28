ba8813dda16f8634599e4a1f0a6bb39d0f23f604
Taeyoon Kim <taeyoon.mathedu@gmail.com>
2023-02-28 15:11:42 +0900

    finished chapter 04

--- do_it_pandas_ch_04.py
+++ do_it_pandas_ch_04.py
@@ -146,7 +146,212 @@ ax.fig.suptitle('Total Bill Density')
 ax.set(xlabel='Total Bill',
        ylabel='Unit Probability')
 
+# %% UserWarning Occured
+ax = plt.subplots()
+ax = sns.distplot(tips['total_bill'], hist=False)
+ax.set_title('Total Bill Density')
+ax.set_xlabel('Total Bill')
+ax.set_ylabel('Unit Probability')
+
+# %% UserWarning Occured
+ax = plt.subplots()
+ax = sns.distplot(tips['total_bill'], rug=True)
+ax.set_title('Total Bill Histogram with Density and Rug Plot')
+ax.set_xlabel('Total Bill')
+
+# %% Count Plot
+ax = plt.subplots()
+ax = sns.countplot(x='day', data=tips)
+ax.set_title('Count of days')
+ax.set_xlabel('Day of the Week')
+ax.set_ylabel('Frequency')
+
+# %% Regression Plot
+ax = plt.subplots()
+ax = sns.regplot(x='total_bill', y='tip', data=tips)
+ax.set_title('Scatterplot of Total Bill and Tip')
+ax.set_xlabel('Total Bill')
+ax.set_ylabel('Tip')
+
+plt
+
+# %% Scatterplot
+ax = plt.subplots()
+ax = sns.regplot(x='total_bill', y='tip', data=tips, fit_reg=False)
+ax.set_title('Scatterplot of Total Bill and Tip')
+ax.set_xlabel('Total Bill')
+ax.set_ylabel('Tip')
+
+plt
+
+# %% jointplot: plot with scatter and histogram
+joint = sns.jointplot(x='total_bill', y='tip', data=tips)
+joint.set_axis_labels(xlabel='Total Bill', ylabel='Tip')
+joint.fig.suptitle('Joint Plot of Total Bill and Tip',
+                   fontsize=10, y=1.03)
+joint
+
+# %% jointplot: hexbin
+hexbin = sns.jointplot(x='total_bill', y='tip', 
+                       data=tips, kind='hex')
+hexbin.set_axis_labels(xlabel='Total Bill', ylabel='Tip')
+hexbin.fig.suptitle('Hexbin Joint Plot of Total Bill and Tip',
+                    fontsize=10, y=1.03)
+
+# %% kdeplot
+ax = plt.subplots()
+ax = sns.kdeplot(data=tips,
+                 x='total_bill',
+                 y='tip',
+                 fill=True)
+ax.set_title('Kernel Density Plot of Total Bill and Tip')
+ax.set_xlabel('Total Bill')
+ax.set_ylabel('tip')
+
+# %% bar plot
+ax = plt.subplots()
+ax = sns.barplot(x='time', y='total_bill', data=tips)
+ax.set_title('Bar plot of average total bill for time of day')
+ax.set_xlabel('Time of day')
+ax.set_ylabel('Average total bill')
+
+# %% box plot
+ax = plt.subplots()
+ax = sns.boxplot(x='time', y='total_bill', data=tips)
+ax.set_title('Boxplot of total bill by time of day')
+ax.set_xlabel('Time of day')
+ax.set_ylabel('Total Bill')
+
+# %% violin plot: to express the variance of the distribution
+ax = plt.subplots()
+ax = sns.violinplot(x='time', y='total_bill', data=tips)
+ax.set_title('Violin plot of total bill by time of day')
+ax.set_xlabel('Time of day')
+ax.set_ylabel('Total Bill')
+
+# %% pair plot
+fig = sns.pairplot(tips)
+fig
+
+# %% map_upper, map_lower
+pair_grid = sns.PairGrid(tips)
+pair_grid = pair_grid.map_upper(sns.regplot)
+pair_grid = pair_grid.map_lower(sns.kdeplot)
+pair_grid = pair_grid.map_diag(sns.distplot, rug=True)
+plt.show()
+
+# %% Draw a Multivariable Graph
+ax = plt.subplots()
+ax = sns.violinplot(x='time', y='total_bill',
+                    hue='sex', data=tips, split=True)
+
+# %% linear model plot
+scatter =  sns.lmplot(x='total_bill', y='tip', data=tips, 
+                      hue='sex', fit_reg=False)
+
 # %%
-plt.subplots(1,1)
-plt.subplot(1,1,1)
-a = sns.displot(data=tips, x="total_bill", kind="kde")
+fig = sns.pairplot(tips, hue='sex')
+
+# %%
+# scatter = sns.lmplot(x='total_bill', y='tip', data=tips,
+#                      fit_reg=False, hue='sex',
+#                     #  scatter_kws={'s': tips['size']*10}
+#                      )
+
+sns.scatterplot(data=tips, x='total_bill', y='tip',
+                hue='sex', size='size')
+
+# %%
+sns.scatterplot(data=tips, x='total_bill', y='tip',
+                hue='sex', size=tips['size'].mul(20))
+
+# %%
+sns.relplot(data=tips, kind='scatter',
+            x='total_bill', y='tip', hue='sex', size='size')
+
+# %%
+sns.relplot(data=tips, kind='scatter',
+            x='total_bill', y='tip', hue='sex', 
+            size=tips['size'].mul(20))
+
+# %% 4 data in 1 plot
+anscombe_plot = sns.lmplot(x='x', y='y', 
+                           data=anscombe, fit_reg=False)
+
+# %% 4 data in 4 plots
+anscombe_plot = sns.lmplot(x='x', y='y',
+                           data=anscombe, fit_reg=False,
+                           col='dataset',
+                           col_wrap=2       # max number of cols
+                           )
+
+# %% Using FacetGrid
+facet = sns.FacetGrid(tips, col='time')
+facet.map(sns.distplot, 'total_bill', rug=True)
+
+# %%
+facet = sns.FacetGrid(tips, col='day', hue='sex')
+facet = facet.map(plt.scatter, 'total_bill', 'tip')
+facet = facet.add_legend()
+
+# %%
+facet = sns.FacetGrid(tips, col='time', row='smoker', hue='sex')
+facet.map(plt.scatter, 'total_bill', 'tip')
+
+# %% 04-4
+ax = plt.subplots()
+ax = tips['total_bill'].plot.hist()
+
+# %%
+fig, ax = plt.subplots()
+ax = tips[['total_bill', 'tip']].plot.hist(
+    alpha=0.5, bins=20, ax=ax
+    )
+
+# %%
+ax = plt.subplots()
+ax = tips['tip'].plot.kde()
+
+# %%
+fig, ax = plt.subplots()
+ax = tips.plot.scatter(x='total_bill', y='tip', ax=ax)
+
+# %%
+fig, ax = plt.subplots()
+ax = tips.plot.hexbin(x='total_bill', y='tip', ax=ax)
+
+# %%
+fig, ax = plt.subplots()
+ax = tips.plot.hexbin(x='total_bill', y='tip', gridsize=10, ax=ax)
+
+# %%
+fig, ax = plt.subplots()
+ax = tips.plot.box(ax=ax)
+
+# %% 04-5 Set Graph Style 
+# before adjust
+fig, ax = plt.subplots()
+ax = sns.violinplot(x='time', y='total_bill',
+                    hue='sex', data=tips, split=True)
+
+# %%
+# after adjust
+sns.set_style('whitegrid')
+fig, ax = plt.subplots()
+ax = sns.violinplot(x='time', y='total_bill', 
+                    hue='sex', data=tips, split=True)
+
+# %% using for-loop 
+fig = plt.figure()
+
+seaborn_styles = ['darkgrid', 'whitegrid', 'dark', 'white', 'ticks']
+
+for idx, style in enumerate(seaborn_styles):
+    plot_position = idx + 1
+    with sns.axes_style(style):
+        ax = fig.add_subplot(2, 3, plot_position)
+        violin = sns.violinplot(x='time', y='total_bill', 
+                                data=tips, ax=ax)
+        violin.set_title(style)
+
+fig.tight_layout()
